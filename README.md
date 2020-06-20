c8d1b2fb1ea7246582f9626476519ec45e81c051
Corbin Robb <corbinrobb@gmail.com>
2020-06-20 14:09:54 -0600

    Start testing and making README

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # DevDesk-Backend
+
+#### Auth
+
+---
+
+| Method | Endpoint                 | Information                                                  |
+| ------ | ------------------------ | ------------------------------------------------------------ |
+| POST   | `/api/register`          | User Registration                                            |
+| POST   | `/api/login`             | User Login                                                   |
+| GET    | `/api/users`             | Returns all users                                            |
+| GET    | `/api/users/:id`         | Returns user by ID                                           |
+| GET    | `/api/users/:id/tickets` | Returns assigned tickets if helper or created tickets if not |
+
+#### Tickets
+
+---
+
+| Method | Endpoint                          | Information                                   |
+| ------ | --------------------------------- | --------------------------------------------- |
+| GET    | `/api/tickets`                    | Returns all tickets                           |
+| GET    | `/api/tickets/:id`                | Returns ticket of id                          |
+| POST   | `/api/tickets/`                   | Create a new ticket                           |
+| PUT    | `/api/tickets/:id`                | Udates ticket with id                         |
+| DELETE | `/api/tickets/:id`                | Deletes ticket with id                        |
+
+
+
+#### Users
+
+---
+
+```js
+{
+  id: INTEGER; // added by database
+  username: STRING; // required , unique - 128 max chars
+  password: STRING; // required - 128 max chars
+  email: STRING; // not required
+  is_helper: BOOLEAN; // defaults to false if not specified
+}
+```
+
+#### Tickets
+
+---
+
+```js
+{
+  id: INTEGER; // added by database
+  title: STRING; // required
+  description: TEXT; // required
+  category: STRING; // not required
+  resolved: BOOLEAN; // defaults to false
+  assigned_to: INTEGER; // defaults to 0 
+  created_at: TIMESTAMP; // The 
+  created_by: INTEGER; // references user id - the users id that created this ticket!
+}
+```
--- api/server.spec.js
+++ api/server.spec.js
@@ -1,5 +1,76 @@
 const request = require('supertest');
 
 const server = require('./server.js');
-const db = require('../database/dbConfig.js');
+const db = require('../data/dbConfig.js');
 
+
+describe('server.js', () => {
+  beforeEach(async () => {
+    await db('users').truncate();
+  })
+
+  it('should use testing environment', () => {
+    expect(process.env.DB_ENV).toBe('testing');
+  })
+
+  describe('should register and return new user', () => {
+    it('should recieve 201 status on success', async () => {
+      const response = await request(server).post('/api/auth/register').send({ username: 'test', password: 'pass' });
+
+      expect(response.status).toEqual(201);
+    })
+
+    it('should return object with username on success', async () => {
+      const response = await request(server).post('/api/auth/register').send({ username: 'test', password: 'pass' });
+
+      expect(response.body).toHaveProperty('username', "test");
+    })
+
+    it('should return a 400 error if missing username', async () => {
+      const response = await request(server).post('/api/auth/register').send({ password: 'pass' });
+
+      expect(response.status).toEqual(400);
+    })
+
+    it('should return a 500 error if username already taken', async () => {
+      await request(server).post('/api/auth/register').send({ username: 'test', password: 'pass' });
+      const response = await request(server).post('/api/auth/register').send({ username: 'test', password: 'pass2' });
+
+      expect(response.status).toEqual(500);
+    })
+  })
+
+  describe('should login user and return token', () => {
+    it('should recieve 200 status on success', async () => {
+      await request(server).post('/api/auth/register').send({ username: 'test', password: 'pass' });
+      const response = await request(server).post('/api/auth/login').send({ username: 'test', password: 'pass' });
+
+      expect(response.status).toEqual(200);
+    })
+
+    it('should return message on success', async () => {
+      await request(server).post('/api/auth/register').send({ username: 'test', password: 'pass' });
+      const response = await request(server).post('/api/auth/login').send({ username: 'test', password: 'pass' });
+
+      expect(response.body).toHaveProperty('username', "test");
+    })
+
+    it('should return token on success', async () => {
+      await request(server).post('/api/auth/register').send({ username: 'test', password: 'pass' });
+      const response = await request(server).post('/api/auth/login').send({ username: 'test', password: 'pass' });
+
+      expect(response.body).toHaveProperty('token');
+    })
+
+    it('should return a 400 error if missing password', async () => {
+      const response = await request(server).post('/api/auth/login').send({ username: 'test' });
+
+      expect(response.status).toEqual(400);
+    })
+  })
+
+
+  describe('/api/tickets endpoint should work as expected', () => {
+  
+  })
+})
