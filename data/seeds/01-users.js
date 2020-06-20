9c2d3493b94fff20186a583dc9be559123256854
Corbin Robb <corbinrobb@gmail.com>
2020-06-20 11:30:18 -0600

    Add knex-cleaner and fix seeds

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -1,26 +1,23 @@
 
 exports.seed = function(knex) {
-  return knex('users').truncate()
-    .then(function () {
-      return knex('users').insert([
-        { username: "Steve",
-          password: "$2a$08$IWkMKT9n1/oBainF6gWH7.nkwe.STComE40nHovPywm.ZgwRqPq3y",
-          is_helper: false,
-          email: "steve@email.com" },
-        { username: "Sara",
-          password: "$2a$08$FUhSouIfgLyLcg3wNQzd4.rDY9Z0M6YZY6gbpVIrajgm6xI93R.e2",
-          is_helper: true,
-          email: "sara@email.com" },
-        { username: "Bob",
-          password: "$2a$08$.KwTOH08PCjKQNOD22zs.eqG9wnlLEUuD449zcI4VtIWmd0kaHz/W",
-          is_helper: false,
-          email: "bob@email.com"
-        }, 
-        { username: "Rachel",
-          password: "$2a$08$0nAZ0m/k8WBpzk.HVoG3ROqyB42BBBanX9nwjEtw2pYrHP9hlUBL2",
-          is_helper: false,
-          email: "rachel@email.com"
-        },
-      ]);
-    });
+  return knex('users').insert([
+    { username: "Steve",
+      password: "$2a$08$IWkMKT9n1/oBainF6gWH7.nkwe.STComE40nHovPywm.ZgwRqPq3y",
+      is_helper: false,
+      email: "steve@email.com" },
+    { username: "Sara",
+      password: "$2a$08$FUhSouIfgLyLcg3wNQzd4.rDY9Z0M6YZY6gbpVIrajgm6xI93R.e2",
+      is_helper: true,
+      email: "sara@email.com" },
+    { username: "Bob",
+      password: "$2a$08$.KwTOH08PCjKQNOD22zs.eqG9wnlLEUuD449zcI4VtIWmd0kaHz/W",
+      is_helper: false,
+      email: "bob@email.com"
+    }, 
+    { username: "Rachel",
+      password: "$2a$08$0nAZ0m/k8WBpzk.HVoG3ROqyB42BBBanX9nwjEtw2pYrHP9hlUBL2",
+      is_helper: false,
+      email: "rachel@email.com"
+    },
+  ]);
 };
--- data/seeds/02-tickets.js
+++ data/seeds/02-tickets.js
@@ -1,47 +1,44 @@
 
 exports.seed = function(knex) {
-  return knex('tickets').truncate()
-    .then(function () {
-      return knex('tickets').insert([
-        { title: "Getting strange error",
-          description: "Couldn't get index file to open",
-          tried: "Googling it",
-          category: "Web",
-          created_by: 1,
-          assigned_to: 0
-        },
-        {
-          title: "Cant sign in to github",
-          description: "Tried to sign in but it was not working",
-          tried: "Googling it and asking a friend",
-          category: "Github",
-          created_by: 1,
-          assigned_to: 2
-        },
-        {
-          title: "Need help writing a for loop",
-          description: "It keeps crashing my browser",
-          tried: "Nothing",
-          category: "Javascript",
-          created_by: 3,
-          assigned_to: 0
-        },
-        {
-          title: "Dropped my sandwich on the floor",
-          description: "Was trying to code an app and I stood up and knocked my plate on the floor",
-          tried: "Using the force",
-          category: "Food",
-          created_by: 3,
-          assigned_to: 0
-        },
-        {
-          title: "What is CORS",
-          description: "I dont understand what cors is and I need help ASAP",
-          tried: "Google",
-          category: "CORS",
-          created_by: 4,
-          assigned_to: 2
-        }
-      ]);
-    });
+  return knex('tickets').insert([
+    { title: "Getting strange error",
+      description: "Couldn't get index file to open",
+      tried: "Googling it",
+      category: "Web",
+      created_by: 1,
+      assigned_to: 0
+    },
+    {
+      title: "Cant sign in to github",
+      description: "Tried to sign in but it was not working",
+      tried: "Googling it and asking a friend",
+      category: "Github",
+      created_by: 1,
+      assigned_to: 2
+    },
+    {
+      title: "Need help writing a for loop",
+      description: "It keeps crashing my browser",
+      tried: "Nothing",
+      category: "Javascript",
+      created_by: 3,
+      assigned_to: 0
+    },
+    {
+      title: "Dropped my sandwich on the floor",
+      description: "Was trying to code an app and I stood up and knocked my plate on the floor",
+      tried: "Using the force",
+      category: "Food",
+      created_by: 3,
+      assigned_to: 0
+    },
+    {
+      title: "What is CORS",
+      description: "I dont understand what cors is and I need help ASAP",
+      tried: "Google",
+      category: "CORS",
+      created_by: 4,
+      assigned_to: 2
+    }
+  ]);
 };
